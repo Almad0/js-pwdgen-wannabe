02ebf5a65195868c972f327ff3c587925ca959d7
Alessandro <internicola.alessandro@gmail.com>
2020-11-12 17:13:05 +0100

    Esercizio completo + BONUS
    Aggiunto del css con una piccola animation, dato colori e dimensioni al testo, evidenziata la var UserPassword

---
+++ BONUS/assets/js/main.js
@@ -0,0 +1,17 @@
+var Saluti = "Benvenuto in questo scarsissimo generatore di password";
+alert(Saluti);
+
+alert("Inserisci i dati richiesti, grazie");
+
+var UserName = prompt("Dimmi il tuo nome.");
+
+var UserSurname = prompt("Qual è il tuo cognome?");
+
+var UserColor = prompt("Ora dimmi il tuo colore preferito, per favore.");
+
+var UserPassword = UserName + UserSurname + UserColor + "20";
+
+alert("Grazie, elaboro la richiesta. Premi ok");
+
+document.getElementById("Pass").innerHTML = UserPassword;
+document.getElementById("Name").innerHTML = "Ciao " + UserName;
